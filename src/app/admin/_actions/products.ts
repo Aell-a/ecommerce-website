b9f7535b766ad9a4098a1f8b70a17f49ef96ffc6
Mordremoth <wantedlordd@gmail.com>
2024-07-29 17:14:37 +0300

    day 6: admin product page improvements bugflxes

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -3,6 +3,7 @@
 import db from "@/db/db";
 import { z } from "zod";
 import fs from "fs/promises";
+import { notFound, redirect } from "next/navigation";
 
 const fileSchema = z.instanceof(File, { message: "Required" });
 const imageSchema = fileSchema.refine(
@@ -16,7 +17,7 @@ const addSchema = z.object({
   image: imageSchema.refine((file) => file.size > 0, "Required"),
 });
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
   const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
   if (result.success === false) {
     return result.error.formErrors.fieldErrors;
@@ -27,23 +28,35 @@ export async function addProduct(formData: FormData) {
 
   await fs.mkdir("public/products", { recursive: true });
 
-  const imagePath = `products/${crypto.randomUUID()}-${data.image.name}`;
+  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
   const arrayBuffer = await data.image.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
 
-  console.log("Attempting to write file at path:", `public/${imagePath}`);
-
   const bufferData: Uint8Array = new Uint8Array(buffer);
 
-  await fs.writeFile(`public/${imagePath}`, bufferData);
-  console.log("File written successfully");
+  await fs.writeFile(`public${imagePath}`, bufferData);
 
   await db.product.create({
     data: {
+      isAvailable: false,
       name: data.name,
       description: data.description,
       priceInCents: data.priceInCents,
       imagePath,
     },
   });
+
+  redirect("/admin/products");
+}
+
+export async function toggleProductAvailability(
+  id: string,
+  isAvailable: boolean
+) {
+  await db.product.update({ where: { id }, data: { isAvailable } });
+}
+
+export async function deleteProduct(id: string) {
+  const product = await db.product.delete({ where: { id } });
+  if (product == null) return notFound();
 }
